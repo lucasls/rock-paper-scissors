cfb7ab1153a8e7a8d39512b9d44eab1df9530c13
Lucas Laurindo dos Santos <lucas.ls@mytaxi.com>
2020-04-25 13:59:41 +0200

    Initial project structure

---
+++ build.gradle.kts
@@ -0,0 +1,23 @@
+plugins {
+    kotlin("jvm") version "1.3.72"
+}
+
+group = "com.github.lucasls.rock-paper-scissors"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+}
+
+tasks {
+    compileKotlin {
+        kotlinOptions.jvmTarget = "11"
+    }
+    compileTestKotlin {
+        kotlinOptions.jvmTarget = "11"
+    }
+}
